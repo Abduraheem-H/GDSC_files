f263eb07810df0d821170453dbe9d8cbb344d7ae
Abduraheem-H <abdurahim.muzemil@aait.edu.et>
2024-07-22 04:45:12 +0300

    Time: 941 ms (27.29%) | Memory: 21.9 MB (6.66%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,29 +1,25 @@
-from typing import List
-
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        soln = []
         nums.sort()
-
+        triplets = {}
+        
         for i in range(len(nums) - 2):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-
+            
             target = -nums[i]
-            left, right = i + 1, len(nums) - 1
-
-            while left < right:
-                if nums[left] + nums[right] == target:
-                    soln.append([nums[i], nums[left], nums[right]])
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
-                    left += 1
-                    right -= 1
-                elif nums[left] + nums[right] < target:
-                    left += 1
-                else:
-                    right -= 1
-
-        return soln
+            complements = {}
+            
+            for j in range(i + 1, len(nums)):
+                complement = target - nums[j]
+                
+                if complement in complements:
+                    triplet = [nums[i], nums[complements[complement]], nums[j]]
+                    triplet.sort()  # Sort to ensure consistent ordering
+                    triplet_key = tuple(triplet)
+                    if triplet_key not in triplets:
+                        triplets[triplet_key] = triplet
+                
+                complements[nums[j]] = j
+        
+        return list(triplets.values())

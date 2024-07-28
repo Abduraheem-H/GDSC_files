1a1e393cdd6f0d8b4400ba29ba4f53bd59c8963e
Abduraheem-H <abdurahim.muzemil@aait.edu.et>
2024-07-29 00:43:59 +0300

    Time: 107 ms (29.83%) | Memory: 16.5 MB (73.95%) - LeetSync

--- 1873-longest-nice-substring/longest-nice-substring.py
+++ 1873-longest-nice-substring/longest-nice-substring.py
@@ -1,21 +1,20 @@
 class Solution:
     def longestNiceSubstring(self,s: str) -> str:
-        # Dynamic sliding window approach
-        left = 0
+        def is_valid(substr: str) -> bool:
+            # using two pointer to loop over all possible substrings
+            unique_char=set(substr)
+            for char in unique_char:
+                if char.lower() not in unique_char or char.upper() not in unique_char:
+                    return False
+            return True
+
         maxi = ""
+        n = len(s)
 
-        while left < len(s):
-            right = len(s)
-            while right > left:
-                #substr = s[left:right]
-                if all(
-                    char.lower() in s[left:right] and char.upper() in s[left:right] for char in s[left:right]
-                ) and len(s[left:right]) > len(maxi):
-                    maxi = s[left:right]
-                right -= 1
-            left += 1
+        for left in range(n):
+            for right in range(left + 1, n + 1):
+                substr = s[left:right]
+                if is_valid(substr) and len(substr) > len(maxi):
+                    maxi = substr
+
         return maxi
-
-
-# print(Solution.longestNiceSubstring("XAyaAx"))  # Output: Aaa
-# print("Here")

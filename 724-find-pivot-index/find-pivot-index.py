312285391850bc20a4580d3e28bc9834f0ea2e67
Abduraheem-H <abdurahim.muzemil@aait.edu.et>
2024-08-04 18:52:40 +0300

    Time: 119 ms (74.60%) | Memory: 17.7 MB (80.39%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -1,23 +1,13 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-
-
-        prefix_sum = [0] * len(nums)
-        prefix_sum[0] = nums[0]
-
-        for i in range(1, len(nums)):
-            prefix_sum[i] = nums[i] + prefix_sum[i - 1]
-
-        total_sum = prefix_sum[-1]
-
+        total_sum = sum(nums)
+        left_sum = 0
+        
         for i in range(len(nums)):
-            left_sum = prefix_sum[i] - nums[i]
-            right_sum = total_sum - prefix_sum[i]
-
-            if left_sum == right_sum:
+            if left_sum == (total_sum - left_sum - nums[i]):
                 return i
-
+            left_sum += nums[i]
+        
         return -1
 
-
-#print(Solution().pivotIndex([1,7,3,6,5,6]))  
+#print(Solution().pivotIndex([2, 1, -1]))  
